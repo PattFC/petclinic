e227495a7a2c3b18f5688e043c8bbcceeca5fea7
admin2 <admin2@petclinic3.myguest.virtualbox.org>
2025-05-06 21:51:57 +0200

    upload monitorization

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-    backend "s3" {
-        bucket         	   = "..."
-        key                = "terraform.tfstate"
-        region         	   = "eu-north-1"
-        encrypt        	   = true
-        dynamodb_table     = "..."
-    }
-}
